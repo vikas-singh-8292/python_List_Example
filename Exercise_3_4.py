7714989b955655e940a8f0e867663cd51625dcfb
Vikas singh satendra <117450754+vikas-singh-8292@users.noreply.github.com>
2022-12-11 17:14:02 +0530

    Add files via upload

---
+++ Exercise_3_4.py
@@ -0,0 +1,17 @@
+# Question 1 :If you could invite anyone, living or deceased, to dinner, who 
+# would you invite? Make a list that includes at least three people you’d like to 
+# invite to dinner. Then use your list to print a message to each person, inviting 
+# them to dinner.
+
+
+
+guest = ['Vikash','Ram','Shyam']
+food = ['paneer tikka', 'khir',"poori sabji"]
+guest_inviting = 'Vikash'
+print("I am inviting " + guest_inviting.title()+" "+"for"+" "+food[0])
+
+guest_inviting = guest[1]
+print("I am inviting " + guest_inviting.title()+" "+"for"+" "+food[1])
+
+guest_inviting = guest[2]
+print("I am inviting " + guest_inviting.title()+" "+"for"+" "+food[2])
